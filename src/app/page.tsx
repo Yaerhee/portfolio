234ee743b6e21a4ec90563c5dd65c7006934e758
yaerhee <yaerhee@naver.com>
2024-06-17 23:54:11 +0900

    feat: add header and color details on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import MainLayout from '@/layouts/MainLayout'
+import {Divider, Flex, Heading, Text} from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    // <main className={styles.main}>
+    <main>
       <MainLayout>
-        <></>
+        <Flex className="profile-content" w="full" maxW="71.25rem" p="8.5rem 2rem 4rem" m="0 auto" justifyContent="center" alignItems="center" flexDirection="column">
+            <Heading fontSize="4rem" textAlign="center" color="white">안녕하세요, <br/>프론트엔드 개발자 이예리 입니다.</Heading>
+            <Flex w="50px" py={2}>
+                <Divider my={10} borderWidth="3px" />
+            </Flex>
+            <Text color="rgba(255, 255, 255, 0.7)" fontSize="26px" fontWeight="500" textAlign="center">
+                사람들에게 도움을 주는 것에 보람을 느끼는,<br/>
+                3년차 프론트엔드 개발자 이예리 입니다.
+            </Text>
+        </Flex>
       </MainLayout>
     </main>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
-import { Box, Flex } from '@chakra-ui/react'
+import {Box, Flex, Heading, Text} from '@chakra-ui/react'
 
 const Header = () => {
   return (
-    <Flex>
-      <Box>Test!</Box>
+    <Flex pos="fixed" top={0} right={0} left={0} my={2} bgColor="transparent" color="rgba(255, 255, 255, .65)">
+        <Flex w="full" maxW="71.25rem" p="1.25rem 2rem" m="0 auto" justifyContent="space-between" alignItems="center">
+            <Heading>Yaerhee's Portfolio</Heading>
+            <Flex float="right" gap={6}>
+                <Box>
+                    <Text fontSize="24px" fontWeight="500">About</Text>
+                </Box>
+                <Box>
+                    <Text fontSize="24px" fontWeight="500">Skills</Text>
+                </Box>
+                <Box>
+                    <Text fontSize="24px" fontWeight="500">Projects</Text>
+                </Box>
+                <Box>
+                    <Text fontSize="24px" fontWeight="500">Career</Text>
+                </Box>
+            </Flex>
+        </Flex>
     </Flex>
   )
 }
