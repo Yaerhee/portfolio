67c64b3762c2e146f67ed9fb9507a38d07acac26
yaerhee <yaerhee@naver.com>
2024-06-18 16:19:01 +0900

    feat: new css + layout with Chakra + ReactIcons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
+'use client'
 import MainLayout from '@/layouts/MainLayout'
-import {Divider, Flex, Heading, Text} from "@chakra-ui/react";
+import { Button, Divider, Flex, Heading, Text } from '@chakra-ui/react'
+import About from '@/pages/About'
+import './globals.css'
+import { ChevronDownIcon } from '@chakra-ui/icons'
 
 export default function Home() {
   return (
     // <main className={styles.main}>
-    <main>
+    <main className='main-page'>
       <MainLayout>
-        <Flex className="profile-content" w="full" maxW="71.25rem" p="8.5rem 2rem 4rem" m="0 auto" justifyContent="center" alignItems="center" flexDirection="column">
-            <Heading fontSize="4rem" textAlign="center" color="white">안녕하세요, <br/>프론트엔드 개발자 이예리 입니다.</Heading>
-            <Flex w="50px" py={2}>
-                <Divider my={10} borderWidth="3px" />
+        <Flex className='profile-content' pos='relative' w='full' p='8.5rem 2rem 4rem' m='0 auto'
+              justifyContent='center' alignItems='center' flexDirection='column'>
+          <Heading fontSize='4rem' textAlign='center' color='white'>안녕하세요, <br />프론트엔드 개발자 이예리 입니다.</Heading>
+          <Flex w='50px' py={2}>
+            <Divider my={10} borderWidth='3px' borderColor='orange.200' />
+          </Flex>
+          <Text color='rgba(255, 255, 255, 0.7)' fontSize='26px' fontWeight='500' textAlign='center'>
+            사람들에게 도움을 주는 것에 보람을 느끼는,<br />
+            3년차 프론트엔드 개발자 이예리 입니다.
+          </Text>
+          <Button my={10} pl={12} pr={8} colorScheme='orange' background='orange.300' borderRadius={40} height='70px'>
+            <Flex gap={4} alignItems="center">
+              <Text fontSize="20px">About Me</Text>
+              <ChevronDownIcon boxSize={8} />
             </Flex>
-            <Text color="rgba(255, 255, 255, 0.7)" fontSize="26px" fontWeight="500" textAlign="center">
-                사람들에게 도움을 주는 것에 보람을 느끼는,<br/>
-                3년차 프론트엔드 개발자 이예리 입니다.
-            </Text>
+          </Button>
         </Flex>
+        <About />
       </MainLayout>
     </main>
   )
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react'
 import Header from '@/components/Header'
+import {Flex} from "@chakra-ui/react";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <Header />
-      {children}
+        <Flex w="full" h="full" flexDirection="column">
+            <Header />
+            {children}
+        </Flex>
     </>
   )
 }
