3aac7e811338eadde88017fc6a784526e81ef562
yaerhee <yaerhee@naver.com>
2024-06-20 01:38:28 +0900

    chore: modify deploy environment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   basePath: '/portfolio',
+  assetPrefix: process.env.NODE_ENV === 'production' ?
+    'https://yaerhee.github.io/portfolio' : '',
 };
 
 export default nextConfig;
